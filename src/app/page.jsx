5b85d812efcf5c7e6753e4484028ccbbfb7983df
BlarQ <collinsogunlala@gmail.com>
2024-07-05 22:43:03 +0100

    fix: length

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -67,7 +67,7 @@ export default function Home() {
               id="text"
               autoComplete="off"
               placeholder="Add New Todo Item..."
-              maxLength={22}
+              maxLength={20}
               onChange={inputChange}
               value={inputValue}
               onKeyDown={handleSubmit}
