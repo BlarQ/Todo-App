6a3b38a3b704314a8a65f13335681b8a6b5f7aa8
BlarQ <collinsogunlala@gmail.com>
2024-07-10 16:54:02 +0100

    fix: text color

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,14 +1,15 @@
 import Link from 'next/link'
 
+
 export default function Footer({theme, onThemeChange}) {
 
   return (
     <div className={`w-full flex flex-col justify-center items-center gap-y-4 bottom-0 fixed py-5 ${theme ? 'bg-[whitesmoke] text-gray-400 font-semibold' : 'bg-[#171926]'}`}>
-        <div className={`flex sm:hidden items-center justify-center px-7 py-4 gap-7 bg-[#393b49] w-[85%] rounded-md text-white ${theme ? 'bg-white text-[#0c0c0c] font-semibold shadow-sm' : ''}`}>
+        <div className={`flex sm:hidden items-center justify-center px-7 py-4 gap-7 bg-[#393b49] w-[85%] rounded-md text-white ${theme ? 'bg-white text-[#9e9fa1] font-semibold shadow-sm' : ''}`}>
 
-            <Link href='/'>All</Link>
-            <Link href='/'>Active</Link>
-            <Link href='/'>Completed</Link>
+            <Link className='text-[#9e9fa1]' href='/'>All</Link>
+            <Link className='text-[#9e9fa1]' href='/'>Active</Link>
+            <Link className='text-[#9e9fa1]' href='/'>Completed</Link>
         </div>
         <p>Drag and drop to reorder list</p>
     </div>
